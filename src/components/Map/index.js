e7a74084e563bc36ee59b0a6285337bd555d78ac
denisprado <denisforigo@gmail.com>
2019-10-03 16:52:03 -0300

    map in progress

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -8,23 +8,26 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 function Map() {
   const projects = useSelector((state) => state.projects);
 
-  const [latitudes, setLatitudes] = useState(null);
+  const [lat, setLat] = useState(-22.83);
+  const [lng, setLng] = useState(-47.09);
 
   useEffect(() => {
-    const arrayLat = projects.data.map((project) => parseFloat(project.lat));
-    const maxArrayAbs = arrayLat.map((lat) => Math.abs(lat));
-    const maxArray = Math.max(...maxArrayAbs);
+    const arrayLat = projects.data.map((project) => Number(project.lat));
+    const arrayLng = projects.data.map((project) => Number(project.lng));
+    const maxArrayLatAbs = arrayLat.map((lat) => Math.abs(lat));
+    const maxArrayLngAbs = arrayLng.map((lng) => Math.abs(lng));
+    const maxLat = Math.max(...maxArrayLatAbs);
+    const maxLng = Math.max(...maxArrayLngAbs);
 
-    setLatitudes(maxArray);
+    setLat(maxLat);
+    setLng(maxLng);
   }, [projects.data]);
-
-  console.log('Latitudes: ' + latitudes);
 
   const [viewport, setViewPort] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
-    latitude: -22.83,
-    longitude: -47.09,
+    latitude: lat,
+    longitude: lng,
     zoom: 14,
   });
 
@@ -38,33 +41,34 @@ function Map() {
   //   await openModal({ latitude, longitude });
   // };
 
-  console.log(projects);
   return (
-    <MapGL
-      {...viewport}
-      mapStyle="mapbox://styles/mapbox/dark-v8"
-      onViewportChange={_onViewportChange}
-      // onClick={this.handleClickMap}
-      mapboxApiAccessToken="pk.eyJ1IjoiZGVuaXNmb3JpZ28iLCJhIjoiY2p2b2Q1eTV6MThwYzN5bnR4Z3BhaHMxMSJ9.fv6R6cp122BKk6B781-z2A"
-    >
-      {projects.data.map((project) => (
-        <Marker
-          key={project.id}
-          latitude={parseFloat(project.lat)}
-          longitude={parseFloat(project.lng)}
-          // onClick={this.handleMapClick}
-          captureClick
-        >
-          <img
-            style={{ borderRadius: '50%' }}
-            src={`https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=${project.title}`}
-            alt={project.title}
-            width="50px"
-            height="50px"
-          />
-        </Marker>
-      ))}
-    </MapGL>
+    projects && (
+      <MapGL
+        {...viewport}
+        mapStyle="mapbox://styles/denisforigo/ck1atos062i7o1cnb3hrssml2"
+        onViewportChange={_onViewportChange}
+        // onClick={this.handleClickMap}
+        mapboxApiAccessToken="pk.eyJ1IjoiZGVuaXNmb3JpZ28iLCJhIjoiY2p2b2Q1eTV6MThwYzN5bnR4Z3BhaHMxMSJ9.fv6R6cp122BKk6B781-z2A"
+      >
+        {projects.data.map((project) => (
+          <Marker
+            key={project.id}
+            latitude={parseFloat(project.lat)}
+            longitude={parseFloat(project.lng)}
+            // onClick={this.handleMapClick}
+            captureClick
+          >
+            <img
+              style={{ borderRadius: '50%' }}
+              src={`https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=${project.title}`}
+              alt={project.title}
+              width="50px"
+              height="50px"
+            />
+          </Marker>
+        ))}
+      </MapGL>
+    )
   );
 }
 
